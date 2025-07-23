ea39eaa823a8a540a68c889f60c61ac69a1f1f3c
Harsh-hbtu <harsh.23@infinitelocus.com>
2025-07-23 21:15:39 +0530

    project services added

--- src/main/java/com/harsh/projectmanagementsystem/service/ProjectService.java
+++ src/main/java/com/harsh/projectmanagementsystem/service/ProjectService.java
@@ -1,11 +1,32 @@
 package com.harsh.projectmanagementsystem.service;
 
+import java.util.*;
+
 import com.harsh.projectmanagementsystem.modal.Project;
+
+import com.harsh.projectmanagementsystem.modal.User;
+import com.harsh.projectmanagementsystem.modal.Chat;
 
 
 public interface ProjectService {
 
 
-     Project createProject(Project project, Long userID)throws Exception;
+     Project createProject(Project project, User user)throws Exception;
+
+     List<Project>getProjectByTeam(User user, String category, String tag) throws Exception;
+
+     Project getProjectById(Long ProjectId) throws Exception;
+
+     void deleteProject(Long projectId, Long userId) throws Exception;
+
+     Project updateProject(Project updatedProject, Long id) throws Exception;
+
+     Void addUserToProject(Long projectId, Long userId) throws Exception;
+
+     void removeUserFromProject(Long projectId, Long userId) throws Exception;
+
+     Chat getChatByProjectId(Long projectId) throws Exception;
+
+
     
 } 
